5fa16af999ad373a42adeb73d34e4801717d3167
Jason Kim <junchkim@ebay.com>
2018-11-15 17:33:34 +0900

    wip

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -19,9 +19,37 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
-  property("gen1") = forAll { (h: H) =>
+  def toStream(h: H): Stream[A] = {
+    if (isEmpty(h)) Stream.Empty
+    else findMin(h) #:: toStream(deleteMin(h))
+  }
+
+  property("gen1") = forAll { h: H =>
     val m = if (isEmpty(h)) 0 else findMin(h)
     findMin(insert(m, h)) == m
   }
 
+  property("min1") = forAll { a: Int =>
+    val h = insert(a, empty)
+    findMin(h) == a
+  }
+
+  property("min2") = forAll { (a: Int, b: Int) =>
+    val h = insert(a, insert(b, empty))
+    findMin(h) == a.min(b)
+  }
+
+  property("min3") = forAll { (a: Int, b: Int, c: Int) =>
+    val h = insert(a, insert(b, insert(c, empty)))
+    findMin(h) == a.min(b).min(c)
+  }
+
+  property("minempty") = forAll { a: Int =>
+    val h = deleteMin(insert(a, empty))
+    isEmpty(h)
+  }
+
+  property("sorted") = forAll { h: H =>
+    toStream(h).sliding(2).forall { case Seq(x, y) => x <= y }
+  }
 }
